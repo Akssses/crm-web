d91452a8b448e7abd3db325258cbba2897eb01da
Akssses <itshnik07@gmail.com>
2025-11-14 17:56:06 +0600

    fixed

---
+++ src/app/admin/dashboard/page.jsx
@@ -0,0 +1,5 @@
+import { Dashboard } from "@/modules/admin";
+
+export default function DashboardPage() {
+  return <Dashboard />;
+}
--- src/ui/index.js
+++ src/ui/index.js
@@ -4,3 +4,4 @@ export { default as Select } from "./Select/Select";
 export { default as Modal } from "./Modal/Modal";
 export { default as Container } from "./Container/Container";
 export { default as Switch } from "./Switch/Switch";
+export { default as Checkbox } from "./Checkbox/Checkbox";
